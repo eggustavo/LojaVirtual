a1340c5c0d11664cff14a688017da2fc80be632c
eggustavo <eggustavo@hotmail.com>
2017-09-23 08:46:19 -0300

    Update SwaggerConfig.cs
    Ajustes

--- LojaVirtual.WebApi/App_Start/SwaggerConfig.cs
+++ LojaVirtual.WebApi/App_Start/SwaggerConfig.cs
@@ -23,43 +23,6 @@ namespace LojaVirtual.WebApi
                     })
                     .Description("API Loja Virtual")
                     .TermsOfService("Nenhum");
-
-                //c.OAuth2("oauth2")
-                //    .Description("OAuth2 Implicit Grant")
-                //    .Flow("implicit")
-                //    .AuthorizationUrl(Helpers.GetIssuerUri() + "/connect/authorize")
-                //    .Scopes(scopes =>
-                //    {
-                //        scopes.Add("sampleapi", "try out the sample api");
-                //    });
-
-
-                /*
-                c.OAuth2("oauth2")
-                    .Description("OAuth2 Implicit Grant")
-                    .Flow("implicit")
-                    .AuthorizationUrl("http://localhost:58582/api/identity/token")
-                    .TokenUrl("http://localhost:58582/api/identity/token")
-                    .Scopes(scopes =>
-                    {
-                        //scopes.Add("read", "Read access to protected resources");
-                        //scopes.Add("write", "Write access to protected resources");
-                        scopes.Add("sampleapi", "try out the sample api");
-                    });
-
-                c.OperationFilter<SwaggerMiddleware>();
-
-                c.BasicAuth("basic").Description("Bearer Token Authentication");
-                */
-
-
-
-            })
-            .EnableSwaggerUi(c =>
-            {
-                /*
-                c.EnableOAuth2Support("sampleapi", "samplerealm", "Swagger UI");
-                */
             });
         }
     }
